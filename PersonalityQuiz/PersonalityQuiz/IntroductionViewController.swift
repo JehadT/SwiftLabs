cd7a42dab2ba07660d75ae1ad02bd990bbff2f52
Jehad <jjehad.tq1@gmail.com>
2023-01-26 00:40:32 +0300

    iOS App

---
+++ PersonalityQuiz/PersonalityQuiz/IntroductionViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Jee on 28/06/1444 AH.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    @IBAction func unwindToQuizIntroduction(segue: UIStoryboardSegue) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
